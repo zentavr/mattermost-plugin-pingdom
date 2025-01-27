ee8262d8bbec6cbc9b3bd6c7434d75787019d3c2
Andrey Miroshnichenko <zentavr@gmail.com>
2025-01-27 01:33:29 +0100

    Adding little-bit default settings for mattermost while developing

--- build/custom.mk
+++ build/custom.mk
@@ -16,3 +16,30 @@ compose-down:
 .PHONY: compose-ps
 compose-ps:
 	cd $(DEVCONTAINER_DIR) && docker compose ps
+
+## Clean removes all build artifacts of the server.
+.PHONY: clean-server
+clean-server:
+	@echo Removing everything in dist/
+	rm -fr dist/
+ifneq ($(HAS_SERVER),)
+	@echo Removing everything for server
+	rm -fr server/coverage.txt
+	rm -fr server/dist
+else
+	@echo There is no server part in the source code
+endif
+
+## Clean removes all build artifacts of the server.
+.PHONY: clean-webapp
+clean-webapp:
+	@echo Removing everything in dist/
+	rm -fr dist/
+ifneq ($(HAS_WEBAPP),)
+	@echo Removing everything for web application
+	rm -fr webapp/junit.xml
+	rm -fr webapp/dist
+	rm -fr webapp/node_modules
+else
+	@echo There is no web application in the source code
+endif
