57472b6b7dd34867b1a26177a5b57ff83096537d
Andrey Miroshnichenko <zentavr@gmail.com>
2025-01-30 01:25:21 +0100

    Removing the licence remarks from the files which had been completely rewritten

--- webapp/src/components/admin_settings/sections/general_settings.tsx
+++ webapp/src/components/admin_settings/sections/general_settings.tsx
@@ -1,6 +1,3 @@
-// Copyright (c) 2020-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
 import React from 'react';
 import {useIntl} from 'react-intl';
 import {SectionTitle} from 'src/components/admin_settings/common';
